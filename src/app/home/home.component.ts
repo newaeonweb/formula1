320a3c72199fcacc8fcb61e6fe9b82ad590cf9d7
newaeonweb <fernando@newaeonweb.com.br>
2017-08-21 22:54:23 -0300

    listagem na home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,26 @@ import { HomeService } from "./home.service";
 export class HomeComponent implements OnInit {
 
   public result: any;
+  public champions = [];
 
   constructor(private homeService: HomeService, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.homeService.getData().subscribe(data => {
-      this.result = data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'];
-      console.log(this.result);
-    }, err => {
-      console.log('Something went wrong!' + JSON.stringify(err));
-    });
+
+    let years = ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'];
+    for (let i = 0; i < years.length; i++) {
+
+      let year = years[i];
+
+      this.homeService.getData(year).subscribe(data => {
+        this.champions.push(data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'][0]);
+        console.log(this.champions);
+      }, err => {
+        console.log('Something went wrong!' + JSON.stringify(err));
+      });
+
+    }
+
 
   }
 
