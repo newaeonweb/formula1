c7df47bfbba91eb0e4adeaa2997bc89f623fe8dc
newaeonweb <fernando@newaeonweb.com.br>
2017-08-22 13:03:05 -0300

    adicionado not found style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,12 @@ export class HomeComponent implements OnInit {
 
   public result: any;
   public champions = [];
+  isLoading = false;
 
   constructor(private homeService: HomeService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.isLoading = true;
 
     let years = ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'];
     for (let i = 0; i < years.length; i++) {
@@ -22,9 +24,11 @@ export class HomeComponent implements OnInit {
       let year = years[i];
 
       this.homeService.getData(year).subscribe(data => {
+        this.isLoading = false;
         this.champions.push(data['MRData']['StandingsTable']['StandingsLists'][0]);
         console.log(this.champions);
       }, err => {
+        this.isLoading = false;
         console.log('Something went wrong!' + JSON.stringify(err));
       });
 
