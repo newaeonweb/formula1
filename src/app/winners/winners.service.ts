7d3f8000bdff2f93896a80aaa7ef2217902a1ab9
newaeonweb <fernando@newaeonweb.com.br>
2017-09-23 10:59:43 -0300

    fixed double quoutes

--- src/app/winners/winners.service.ts
+++ src/app/winners/winners.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class WinnersService {
+  private url: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getData() {
-    return [1,2]
+
+  getData(year) {
+    this.url = 'http://ergast.com/api/f1/' + year + '/driverStandings.json?limit=400&offset=0';
+
+    return this.http.get(this.url);
+
   }
 
 }
